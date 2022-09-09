cee5ed20718e791e860b7728b9aeba45326186ae
michaelokoroike <39680418+michaelokoroike@users.noreply.github.com>
2022-09-09 16:19:01 -0700

    Create Mathematical-Operators.py
    File with information on mathematical operators and the usage in Python

---
+++ Python/Mathematical-Operators.py
@@ -0,0 +1,33 @@
+#Information = https://thehelloworldprogram.com/python/python-operators-order-precedence/
+
+# Operators
+
++ #(addition)
+- #(subtraction)
+* #(multiplication)
+/ #(division)
+// #(modulus)
+** #(exponentiation)
+
+
+# Examples
+
+print(1+1) #addition
+print(1-1) #subtraction
+print(1*1) #multiplication
+print(1/1) #division
+print(5//2) #modulus - will print out the remainder of the equation (example = 3 goes into 2 two times, with 1 as the remainder; 1 would be printed)
+print(1**1) #exponentiation
+
+
+# Precedence
+
+![image](https://user-images.githubusercontent.com/39680418/189223877-cf4ea2be-cae0-4c80-ae41-f84751d8c93c.png)
+
+# Order (and how Precedence is involved)
+
+x = 1+2*3 
+print(x) #x = 7
+
+x = (1+2)*3
+print(x) #x = 9
