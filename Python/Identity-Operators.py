94eebf571f7e16ca3909a0aab803ad9a18e791b8
michaelokoroike <39680418+michaelokoroike@users.noreply.github.com>
2022-09-10 16:32:28 -0700

    Create Identity-Operators.py
    Identity operators are used to compare the objects, not if they are equal, but if they are actually the same object, with the same memory location.

---
+++ Python/Identity-Operators.py
@@ -0,0 +1,11 @@
+#Information = https://www.w3schools.com/python/python_operators.asp
+
+#Identity operators are used to compare the objects, not if they are equal, but if they are actually the same object, with the same memory location.
+
+x = "hi"
+y = "bye"
+
+print(x is y) #will print False
+
+print(x is not y) #will print True
+
