0acb5055f5df5b91d740c71158ba502cc0e074f5
michaelokoroike <39680418+michaelokoroike@users.noreply.github.com>
2022-09-10 13:25:47 -0700

    Create Comparison-Operators.py
    Comparison operators are used to compare two values.

---
+++ Python/Comparison-Operators.py
@@ -0,0 +1,18 @@
+#Information: https://www.w3schools.com/python/python_operators.asp
+
+#Comparison operators are used to compare two values:
+
+x = 1
+y = 1
+
+print(x == y) #are x and y equal to each other? Would print True
+
+print(x != y) #are x and y not equal to each other? Would print False
+
+print(x > y) #is x greater than y? Would print False
+
+print(x < y) #is x less than y? Would print False
+
+print(x >= y) #is x greater than or equal to y? Would print True
+
+print(x <= y) #is x less than or equal to y? Would print True
