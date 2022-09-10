87db1c1ef1462ddc4c45814d869f5b83476ec72a
michaelokoroike <39680418+michaelokoroike@users.noreply.github.com>
2022-09-10 12:59:28 -0700

    Create Assignment-Operators.py
    Assignment operators are used to assign values to variables.

---
+++ Python/Assignment-Operators.py
@@ -0,0 +1,29 @@
+#Information = https://www.w3schools.com/python/gloss_python_assignment_operators.asp
+
+#Assignment operators are used to assign values to variables:
+
+x = 5  #same as x = 5
+
+x += 5 #same as x = x + 5
+
+x -= 5 #same as x = x - 5
+
+x *= 5 #same as x = x * 5
+
+x /= 5 #same as x = x / 5
+
+x %= 5 #same as x = x % 5
+
+x //= 5 #same as x = x // 5
+
+x **= 5 #same as x = x ** 5
+
+x &= 5 #same as x = x & 5
+
+x |= 5 #same as x = x | 5
+
+x ^= 5 #same as x = x ^ 5
+
+x >>= 5 #same as x = x >> 5
+
+x <<= 5 #same as x = x << 5
