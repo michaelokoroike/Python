04d3328b1ce2ba0e925675b1f8eed6aed26f7ead
michaelokoroike <39680418+michaelokoroike@users.noreply.github.com>
2022-09-11 23:21:03 -0700

    Create break.py
    Information on the break statement in Python.

---
+++ Python/Control-Statements/break.py
@@ -0,0 +1,16 @@
+#Information: https://www.softwaretestinghelp.com/python/python-control-statements/
+
+'''The break statement is used to terminate the loop containing it, the control of the program will come out of that loop.'''
+
+for char in ‘Python’:
+       if (char == ‘h’):
+           break
+       print(“Current character: “, char
+             
+'''Output:
+
+Current character: P
+
+Current character: y
+
+Current character: t'''
